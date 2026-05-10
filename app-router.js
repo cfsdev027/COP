6f0caba55b89feaf109a5cc4c8341ce9781b6a5f
cfsdev027 <cfsdev.github@gmail.com>
2026-05-10 18:53:38 -0300

    Add SRP section to AppRouter

--- app-router.js
+++ app-router.js
@@ -3,12 +3,22 @@ import {AppSectionController} from './app-section-controller.js';
 import {LoginUI} from './login-ui.js';
 import {SECTION_LOGIN_ID} from './config-login-ui.js';
 
+import {SrpUI} from './srp-ui.js';
+import {SECTION_SRP_ID} from './config-srp-ui.js';
+
 export const AppRouter = {
     login: {
         section_id: SECTION_LOGIN_ID,
         init: function() {
             LoginUI.init();
             AppSectionController.navigateTo(SECTION_LOGIN_ID);
+        }
+    },
+    srp: {
+        section_id: SECTION_SRP_ID,
+        init: function() {
+            SrpUI.init();
+            AppSectionController.navigateTo(SECTION_SRP_ID);
         }
     }
 };
