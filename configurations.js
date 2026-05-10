74587074fc88e49b60e327cfe44daa0b74187326
cfsdev027 <cfsdev.github@gmail.com>
2026-05-10 09:33:11 -0300

    Add Supabase credentials and constants
    Added Supabase credentials and constants for authentication and local storage.

--- configurations.js
+++ configurations.js
@@ -1,1 +1,9 @@
-export const TESTE = 'Aplicação inicializada com sucesso.';
+// SUPABASE CREDENTIALS
+const SUPABASE_URL = "https://sdakvoeythnbfqfgupzf.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkYWt2b2V5dGhuYmZxZmd1cHpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxMDA1MjksImV4cCI6MjA5MzY3NjUyOX0.U13KCcwWUTStp1-k8at9CudflI66uJ8YhMzSErQAlrM";
+
+// AUTHENTICATICATION COOKIE NAME
+const COP_AUTH_COOKIE = 'COP-AUTH-MOCKUP';
+
+// LOCAL STORAGE NAME
+const COP_LS_KEY = 'COP-LS-MOCKUP';
