d380a041275cde8a017d1667b6d2c31a375d26dd
cfsdev027 <cfsdev.github@gmail.com>
2026-05-10 14:39:33 -0300

    Refactor currentActiveSection to a property
    Refactor currentActiveSection to be a property instead of a function.

--- app-section-controller.js
+++ app-section-controller.js
@@ -1,19 +1,12 @@
 import {SECTION_ACTIVE_CLASSNAME } from './config-sections.js';
 
 export const AppSectionController = {
-    currentActiveSection = function() {
-        try {
-            return document.querySelector('.section-base.' + SECTION_ACTIVE_CLASSNAME);
-        } catch(err) {
-            console.log('[AppSectionController.currentActiveSection] ' + err.message);
-            return null;
-        }
-    },
+    currentActiveSection: document.querySelector('.section-base.' + SECTION_ACTIVE_CLASSNAME),
     navigateTo: function(targetId) {
         try {
             const nextSection = document.getElementById(targetId);
             if (!nextSection || nextSection === currentActiveSection) return;
-    
+            
             if (currentActiveSection) {
                 currentActiveSection.classList.remove(SECTION_ACTIVE_CLASSNAME);
             }
