314a17f8f844b3ac9ba7d42530b73657946ea455
cfsdev027 <cfsdev.github@gmail.com>
2026-05-10 10:52:46 -0300

    Rename UserModel to UserDAO and set active to true

---
+++ user-dao.js
@@ -0,0 +1,10 @@
+export const UserDAO = {
+    id: null,
+    created_at: null,
+    username: null,
+    password: null,
+    active: true,
+    document: null,
+    document_type: null,
+    role: null
+}
