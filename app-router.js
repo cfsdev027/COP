2db73c0dfec4d21e5055d79009cdef97a5b1b022
cfsdev027 <cfsdev.github@gmail.com>
2026-05-10 14:33:51 -0300

    Rename Router to AppRouter in app-router.js

--- app-router.js
+++ app-router.js
@@ -3,7 +3,7 @@ import {AppSectionController} from './app-section-controller.js';
 import {LoginUI} from './login-ui.js';
 import {SECTION_LOGIN_ID} from './config-login-ui.js';
 
-export const Router = {
+export const AppRouter = {
     login: {
         section_id: SECTION_LOGIN_ID,
         init: function() => {
