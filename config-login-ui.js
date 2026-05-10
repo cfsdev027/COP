64e21ba7a3e79dbd939f881b09b31495147c6c66
cfsdev027 <cfsdev.github@gmail.com>
2026-05-10 13:37:09 -0300

    Add login section constants to config-login-ui.js

--- config-login-ui.js
+++ config-login-ui.js
@@ -1,1 +1,7 @@
 export const SECTION_LOGIN_ID = 'section-login';
+
+export const SECTION_LOGIN_TITLE_ID = 'login-title';
+export const SECTION_LOGIN_TITLE = 'CONTROLE DE ORDEM DE PRODUÇÃO (OP)';
+
+export const SECTION_LOGIN_FORM_ID = 'form-login';
+export const SECTION_LOGIN_FORM_SUBMIT_ID = 'btn-login-submit';
