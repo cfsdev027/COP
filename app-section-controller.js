12d3c7f6b38331b8c490460472450736143d65f9
cfsdev027 <cfsdev.github@gmail.com>
2026-05-10 13:24:40 -0300

    Add currentActiveSection and navigateTo methods
    Refactor AppSectionController to include currentActiveSection and navigateTo methods.

--- app-section-controller.js
+++ app-section-controller.js
@@ -1,15 +1,30 @@
 import {LoginUI} from './login-ui.js';
 
 export const AppSectionController = {
-    enable: function(section) {
-        switch(section){
-            case 'login':
-              LoginUI.init();
-              break;
-            case 'srp':
-              break;
-          default:
-              throw 'Invalid section';
+    currentActiveSection = function() {
+        try {
+            return document.querySelector('.section-base.active');
+        } catch(err) {
+            console.log('[AppSectionController.currentActiveSection] ' + err.message);
+            return null;
+        }
+    },
+    navigateTo: function(targetId, callback) {
+        try {
+            const nextSection = document.getElementById(targetId);
+            if (!nextSection || nextSection === currentActiveSection) return;
+    
+            if (currentActiveSection) {
+                currentActiveSection.classList.remove('active');
+            }
+    
+            nextSection.classList.add('active');
+            currentActiveSection = nextSection;
+
+            if(typeof callbac === 'function')
+                callback();
+        } catch(err) {
+                console.log('[AppSectionController.navigateTo] ' + err.message);
         }
     }
 }
