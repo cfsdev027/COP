a4033b1fa3bab539593d2bd3b4f941702b9f59ee
cfsdev027 <cfsdev.github@gmail.com>
2026-05-11 07:21:51 -0300

    Await self_authenticate in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import {AppRouter} from './app-router.js';
 import {ServiceAuthentication} from './service-authentication.js';
 
 (async () => {
-    let isAuthenticated = ServiceAuthentication.self_authenticate();
+    let isAuthenticated = await ServiceAuthentication.self_authenticate();
     if (isAuthenticated) {
         AppRouter['srp'].init(); 
     } else {
