a9131ef58976cb9ff9ce92eb08bb5e7373c8f5be
cfsdev027 <cfsdev.github@gmail.com>
2026-05-11 07:31:36 -0300

    Fix typo in authentication service name

--- login-ui.js
+++ login-ui.js
@@ -107,7 +107,7 @@ export const LoginUI = {
         errorElement.hidden = true;
 
         (async () => {
-            let isAuthenticated = await ServiceAuthenticate.authenticate(user,pass);
+            let isAuthenticated = await ServiceAuthentication.authenticate(user,pass);
             if(!isAuthenticated) {
                 errorElement.hidden = false;
                 return;
