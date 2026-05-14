93002189b3c219daf7fc7e245ff91c692e48ad1d
cfsdev027 <cfsdev.github@gmail.com>
2026-05-14 11:31:50 -0300

    Rename SIDEBAR_UI_OPTIONS to DASHBOARD_SIDEBAR_UI_OPTIONS

--- config-dashboard-ui.js
+++ config-dashboard-ui.js
@@ -2,7 +2,7 @@ export const SECTION_DASHBOARD_ID = 'section-dashboard';
 export const SECTION_DASHBOARD_SIDEBAR_ID = 'sidebar';
 export const SECTION_DASHBOARD_SBMC_ID = 'menu-container';
 
-export const SIDEBAR_UI_OPTIONS = [
+export const DASHBOARD_SIDEBAR_UI_OPTIONS = [
     {
         id: 'opt-users',
         text: 'Usuários',
