f593dbb39ac6143b61f7953793667111e9109379
cfsdev027 <cfsdev.github@gmail.com>
2026-05-14 12:22:30 -0300

    Fix formatting of error string in CatchError function

--- catch-error.js
+++ catch-error.js
@@ -1,7 +1,7 @@
 import { ENV } from './configurations.js';
 
 export function CatchError(prefix, err) {
-        const e = typeof err === 'string' ? err : JSON.stringify(err);
+        const e = (typeof err === 'string') ? err : JSON.stringify(err);
         console.log('[' + prefix + ']: ' + e);
       
         if(ENV === 'dev') {
