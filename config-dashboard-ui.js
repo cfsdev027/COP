677092e8df0d22b70a8c296f88ee0dfcd15a9300
cfsdev027 <cfsdev.github@gmail.com>
2026-05-14 11:29:36 -0300

    Add sidebar UI options for user alert

--- config-dashboard-ui.js
+++ config-dashboard-ui.js
@@ -1,3 +1,13 @@
 export const SECTION_DASHBOARD_ID = 'section-dashboard';
 export const SECTION_DASHBOARD_SIDEBAR_ID = 'sidebar';
 export const SECTION_DASHBOARD_SBMC_ID = 'menu-container';
+
+export const SIDEBAR_UI_OPTIONS = [
+    {
+        id: 'opt-users',
+        text: 'Usuários',
+        action: () => {
+            alert('USUÁRIOS');
+        }
+    }
+];
