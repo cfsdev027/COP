73d298cb7dff0ed29045bd19253c1a122bd3a7d6
cfsdev027 <cfsdev.github@gmail.com>
2026-05-22 11:01:04 -0300

    Create config-users-ui.js

---
+++ config-users-ui.js
@@ -0,0 +1,4 @@
+export const USERS_SECTION_ID = 'section-users';
+export const USERS_CONTAINER_ID = 'users-registry-container';
+export const USERS_DEFAULT_TITLE = 'Gerenciamento de Usuários do Sistema';
+
