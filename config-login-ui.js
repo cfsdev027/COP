647de041f6dc0fafbc0cc688198b233fca90e76f
cfsdev027 <cfsdev.github@gmail.com>
2026-06-08 08:26:52 -0300

    Delete config-login-ui.js

--- config-login-ui.js
+++
@@ -1,7 +0,0 @@
-export const SECTION_LOGIN_ID = 'section-login';
-
-export const SECTION_LOGIN_TITLE_ID = 'login-title';
-export const SECTION_LOGIN_TITLE = 'CONTROLE DE ORDEM DE PRODUÇÃO (OP)';
-
-export const SECTION_LOGIN_FORM_ID = 'form-login';
-export const SECTION_LOGIN_FORM_SUBMIT_ID = 'btn-login-submit';
