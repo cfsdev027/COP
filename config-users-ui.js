dd3ce98dea2e2bdcdaada06d12e0ac95e22bc198
cfsdev027 <cfsdev.github@gmail.com>
2026-06-08 08:27:41 -0300

    Delete config-users-ui.js

--- config-users-ui.js
+++
@@ -1,4 +0,0 @@
-export const USERS_SECTION_ID = 'section-users';
-export const USERS_CONTAINER_ID = 'users-registry-container';
-export const USERS_DEFAULT_TITLE = 'Gerenciamento de Usuários do Sistema';
-
