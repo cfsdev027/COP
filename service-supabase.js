a3d6feaf558b4046b7b9a07d0e3aa07ac133189d
cfsdev027 <cfsdev.github@gmail.com>
2026-06-08 08:30:14 -0300

    Delete service-supabase.js

--- service-supabase.js
+++
@@ -1,7 +0,0 @@
-import {SUPABASE_URL,SUPABASE_ANON_KEY} from './configurations.js';
-
-export const ServiceSupabase = {
-    client: function(){
-      return window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-    }
-}
