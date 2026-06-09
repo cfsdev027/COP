41500d96c14b0cd7eb925010b4e28b0b86e453ad
cfsdev027 <cfsdev.github@gmail.com>
2026-06-09 11:41:41 -0300

    Update USERS_DEFAULT_TITLE to 'Usuários'

--- src/js/config/ui/config-users-ui.js
+++ src/js/config/ui/config-users-ui.js
@@ -1,4 +1,3 @@
 export const USERS_SECTION_ID = 'section-users';
 export const USERS_CONTAINER_ID = 'users-registry-container';
-export const USERS_DEFAULT_TITLE = 'Gerenciamento de Usuários do Sistema';
-
+export const USERS_DEFAULT_TITLE = 'Usuários';
