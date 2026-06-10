209f2c7d92a75b2ddda239588877c0939333864a
cfsdev027 <cfsdev.github@gmail.com>
2026-06-10 16:48:14 -0300

    Create UserSchema class for user data management

---
+++ src/js/schemas/user-schema.js
@@ -0,0 +1,75 @@
+import {UserModel} from '../models/user-model.js';
+
+export class UserSchema {
+    constructor(){
+        this.model = UserModel;
+      
+        this.id = {
+            display: false,
+            type: 'text',
+            editable: false
+        };
+      
+        this.createdAt = {
+            display: false,
+            type: 'datetime-local',
+            editable: false
+        };
+      
+        this.username = {
+            display: true,
+            type: 'text',
+            editable: true
+        };
+      
+        this.password = {
+            display: true,
+            type: 'password',
+            editable: true
+        };
+      
+        this.active = {
+            display: true,
+            type: 'checkbox',
+            editable: false
+        };
+      
+        this.document = {
+            display: true,
+            type: 'text',
+            editable: true
+        };
+      
+        this.documentType = {
+            display: true,
+            type: 'select',
+            options: [
+                {
+                  text:'CPF',
+                  value:'CPF'
+                },
+                {
+                  text:'CNPJ',
+                  value:'CNPJ'
+                }
+            ],
+            editable: true
+        };
+      
+        this.role = {
+            display: true,
+            type: 'select',
+            options: [
+                {
+                    text: 'DEFAULT',
+                    value: 'DEFAULT'
+                },
+                {
+                    text: 'ADMIN',
+                    value: 'ADMIN'
+                }
+            ],
+            editable: true
+        };
+    }
+}
