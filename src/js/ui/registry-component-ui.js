86d40fed161c4f534111e6ec656e65865250d687
cfsdev027 <cfsdev.github@gmail.com>
2026-06-11 09:04:37 -0300

    Refactor RegistryComponent with region comments

--- src/js/ui/registry-component-ui.js
+++ src/js/ui/registry-component-ui.js
@@ -10,6 +10,8 @@ export const RegistryComponent = {
     service: null,
     data: null,
 
+    //#region INIT
+
     async initAsync(containerId, title, model, schema, service) {
         try {
             if(!containerId || containerId === null) throw { stack: 'init', message_error: `'containerId' is null or empty.` };
@@ -53,6 +55,10 @@ export const RegistryComponent = {
 
         this.container.append(elHeaderBlock, elGrid);
     },
+
+    //#endregion
+
+    //#region EVENTS
 
     add() {
         const elTr = document.getElementById('data-registry-row');
@@ -105,35 +111,6 @@ export const RegistryComponent = {
         })(); 
     },
 
-    extractData(elTr) {
-        const data = [];
-        const columns = elTr.querySelectorAll('td[data-column-name]');
-  
-        columns.forEach(td => {
-            const prop = td.getAttribute('data-column-name');
-            const elInput = td.querySelector('input');
-    
-            if (elInput) {
-                data.push({ property: prop, value: elInput.value });
-            }
-        });
-  
-        return data;
-    },
-
-    updateData(entry) {
-        const elTr = document.querySelector(`tr[data-id='${entry.id}']`);
-        if(!elTr || elTr === null) return;
-
-        Object.entries(entry).forEach(([p, val]) => {
-            const elTd = elTr.querySelector(`td[data-column-name=${p}]`);
-            if(elTd) {
-                const elInput = elTd.querySelector('input');
-                if(elInput) elInput.value = val;
-            }
-        });
-    },
-
     filter: async function(expression) {
         if(!expression || expression === null || expression.trim() === '') {
             this.data = await this.service.get();
@@ -144,10 +121,177 @@ export const RegistryComponent = {
         this.reloadGridviewContent();
     },
 
-    makeDynamic(params) {
-        const result = {};
-        params.forEach(item => { result[item.property] = item.value; });
-        return result;
+    //#endregion
+
+    //#region MAKE
+
+    makeGridviewFooter() {
+        const elFooter = el('div', ['datagrid-footer', 'd-flex', 'justify-content-between', 'align-items-center', 'flex-wrap', 'gap-3']);
+        
+        const elPagination = el('div', ['pagination-container', 'd-flex', 'gap-1']);
+        const btnPrev = el('button', ['btn-page'], { disabled: true }); btnPrev.innerText = 'Previous';
+        const btnP1 = el('button', ['btn-page']); btnP1.innerText = '1';
+        const btnP2 = el('button', ['btn-page', 'active']); btnP2.innerText = '2';
+        const btnP3 = el('button', ['btn-page']); btnP3.innerText = '3';
+        const btnNext = el('button', ['btn-page']); btnNext.innerText = 'Next';
+        elPagination.append(btnPrev, btnP1, btnP2, btnP3, btnNext);
+
+        const elInfo = el('div', ['d-flex', 'align-items-center', 'gap-3', 'text-muted', 'font-size-sm']);
+        elInfo.innerHTML = `<div>Showing 1 to 2 of 2 entries</div>`;
+
+        elFooter.append(elPagination, elInfo);
+        return elFooter;
+    },
+
+    makeInput(s, p) {
+        let elInput = null;
+
+        switch(s.type) {
+            case 'select':
+                elInput = el('select', ['datagrid-cell-input'], { type: s.type });
+                    
+                let elOpt = el('option');
+                elOpt.value = '';
+                elOpt.text = '';
+
+                elInput.append(elOpt);
+                    
+                if(s.options){
+                    s.options.forEach(opt => {
+                        elOpt = el('option');
+                        elOpt.value = opt.value;
+                        elOpt.innerHTML = opt.text;
+
+                        elInput.append(elOpt);
+                    });
+                }
+                    
+                break;
+            default:
+                elInput = el('input', ['datagrid-cell-input'], { type: s.type });
+                break;
+        }
+
+        if(!s.editable) elInput.disabled = true;
+
+        elInput.setAttribute('data-property-name', p);
+
+        return elInput;
+    },
+
+    makeGridview() {
+        const elCard = el('div', ['card', 'datagrid-card']);
+        const elTableResponsive = el('div', ['table-responsive']);
+        const elTable = el('table', ['table', 'datagrid-table', 'align-middle', 'm-0']);
+        
+        const elThead = this.makeGridviewHeader();
+        const elTbody = this.makeGridviewContent();
+        
+        elTable.append(elThead, elTbody);
+        elTableResponsive.append(elTable);
+        
+        const elFooter = this.makeGridviewFooter();
+        elCard.append(elTableResponsive, elFooter);
+        return elCard;
+    },
+
+    makeGridviewContent() {
+        const elTbody = el('tbody', [], { id: 'gridview-tbody' });
+        this.appendGridviewContentRows(elTbody);
+
+        return elTbody;
+    },
+
+    makeGridviewContentNew() {
+        const elRow = el('tr', [], { id: 'data-registry-row' });
+        Object.entries(new this.model()).forEach(([p, val]) => {
+            const s = this.schema[this.toLowerCamelCase(p)];
+            if(!s || s === null) return;
+
+            const elTd = el('td');
+            if(!s.display) elTd.style.display = 'none';
+            
+            elTd.setAttribute('data-column-type', s.type);
+            elTd.setAttribute('data-column-name', p);
+
+            const elInput = this.makeInput(s, p);
+            
+            elTd.append(elInput);
+            elRow.append(elTd);
+        });
+
+        const elTdActions = el('td', ['text-nowrap', 'datagrid-actions-cell']);
+        const elActionGroup = el('div', ['d-flex', 'gap-1']);
+        
+        const elAdd = el('button', ['btn-action'], { title: 'Adicionar novo registro.' });
+        elAdd.innerHTML = '✅';
+
+        elAdd.addEventListener('click', () => this.add());
+
+        elActionGroup.append(elAdd);
+        elTdActions.append(elActionGroup);
+        elRow.append(elTdActions);
+
+        return elRow;
+    },
+
+    makeGridviewContentRows() {
+        const rows = [];
+        if(!this.data || this.data === null) return rows;
+
+        this.data.forEach((entry) => {
+            const elRow = this.makeGridviewContentRow(entry);
+            rows.push(elRow);
+        });
+
+        return rows;
+    },
+
+    makeGridviewContentRow(entry) {
+        const elRow = el('tr', []);
+        elRow.setAttribute('data-id', entry.id);
+
+        Object.entries(entry).forEach(([p, val]) => {
+            const s = this.schema[this.toLowerCamelCase(p)];
+            if(!s || s === null) return;
+            
+            const elTd = el('td');
+            elTd.setAttribute('data-column-type', s.type);
+            elTd.setAttribute('data-column-name', p);
+            if(!s.display) elTd.style.display = 'none';
+
+            const elInput = this.makeInput(s, p);
+            elInput.value = val;
+            
+            elTd.append(elInput);
+            elRow.append(elTd);
+        });
+
+        const elTdActions = this.makeGridviewContentRowActions(entry);
+        elRow.append(elTdActions);
+
+        return elRow;
+    },
+
+    makeGridviewContentRowActions(entry) {
+        const elTdActions = el('td', ['text-nowrap', 'datagrid-actions-cell']);
+        const elActionGroup = el('div', ['d-flex', 'gap-1']);
+        
+        const btnView = el('button', ['btn-action', 'btn-action-view'], { title: 'Visualizar' });
+        btnView.innerHTML = '🔍';
+        
+        const btnEdit = el('button', ['btn-action', 'btn-action-edit'], { title: 'Salvar' });
+        btnEdit.innerHTML = '✏️';
+        btnEdit.addEventListener('click', () => this.commit(entry.id));
+        
+        const btnDelete = el('button', ['btn-action', 'btn-action-delete'], { title: 'Excluir' });
+        btnDelete.innerHTML = '🗑️';
+        btnDelete.addEventListener('click', () => this.delete(entry.id));
+
+        elActionGroup.append(btnView, btnEdit, btnDelete);
+        elTdActions.append(elActionGroup);
+
+        return elTdActions;
     },
 
     makeGridviewHeader() {
@@ -227,173 +371,38 @@ export const RegistryComponent = {
         return elSearchWrapper;
     },
 
-    makeGridview() {
-        const elCard = el('div', ['card', 'datagrid-card']);
-        const elTableResponsive = el('div', ['table-responsive']);
-        const elTable = el('table', ['table', 'datagrid-table', 'align-middle', 'm-0']);
-        
-        const elThead = this.makeGridviewHeader();
-        const elTbody = this.makeGridviewContent();
-        
-        elTable.append(elThead, elTbody);
-        elTableResponsive.append(elTable);
-        
-        const elFooter = this.makeGridviewFooter();
-        elCard.append(elTableResponsive, elFooter);
-        return elCard;
-    },
+    //#endregion
 
-    makeGridviewContent() {
-        const elTbody = el('tbody', [], { id: 'gridview-tbody' });
-        this.appendGridviewContentRows(elTbody);
+    //#region AUX
 
-        return elTbody;
-    },
-
-    makeGridviewContentNew() {
-        const elRow = el('tr', [], { id: 'data-registry-row' });
-        Object.entries(new this.model()).forEach(([p, val]) => {
-            const s = this.schema[this.toLowerCamelCase(p)];
-            if(!s || s === null) return;
-
-            const elTd = el('td');
-            if(!s.display) elTd.style.display = 'none';
-            
-            elTd.setAttribute('data-column-type', s.type);
-            elTd.setAttribute('data-column-name', p);
-
-            const elInput = this.makeInput(s, p);
-            
-            elTd.append(elInput);
-            elRow.append(elTd);
-        });
-
-        const elTdActions = el('td', ['text-nowrap', 'datagrid-actions-cell']);
-        const elActionGroup = el('div', ['d-flex', 'gap-1']);
-        
-        const elAdd = el('button', ['btn-action'], { title: 'Adicionar novo registro.' });
-        elAdd.innerHTML = '✅';
-
-        elAdd.addEventListener('click', () => this.add());
-
-        elActionGroup.append(elAdd);
-        elTdActions.append(elActionGroup);
-        elRow.append(elTdActions);
-
-        return elRow;
-    },
-
-    makeInput(s, p) {
-        let elInput = null;
-
-        switch(s.type) {
-            case 'select':
-                elInput = el('select', ['datagrid-cell-input'], { type: s.type });
-                    
-                let elOpt = el('option');
-                elOpt.value = '';
-                elOpt.text = '';
-
-                elInput.append(elOpt);
-                    
-                if(s.options){
-                    s.options.forEach(opt => {
-                        elOpt = el('option');
-                        elOpt.value = opt.value;
-                        elOpt.innerHTML = opt.text;
-
-                        elInput.append(elOpt);
-                    });
-                }
-                    
-                break;
-            default:
-                elInput = el('input', ['datagrid-cell-input'], { type: s.type });
-                break;
+    appendGridviewContentRows(container) {
+        container.append(this.makeGridviewContentNew());
+        const rows = this.makeGridviewContentRows();
+        if (rows && rows.length > 0) {
+            rows.forEach(elRow => container.append(elRow));
         }
-
-        if(!s.editable) elInput.disabled = true;
-
-        elInput.setAttribute('data-property-name', p);
-
-        return elInput;
     },
 
-    makeGridviewContentRow(entry) {
-        const elRow = el('tr', []);
-        elRow.setAttribute('data-id', entry.id);
-
-        Object.entries(entry).forEach(([p, val]) => {
-            const s = this.schema[this.toLowerCamelCase(p)];
-            if(!s || s === null) return;
-            
-            const elTd = el('td');
-            elTd.setAttribute('data-column-type', s.type);
-            elTd.setAttribute('data-column-name', p);
-            if(!s.display) elTd.style.display = 'none';
-
-            const elInput = this.makeInput(s, p);
-            elInput.value = val;
-            
-            elTd.append(elInput);
-            elRow.append(elTd);
+    extractData(elTr) {
+        const data = [];
+        const columns = elTr.querySelectorAll('td[data-column-name]');
+  
+        columns.forEach(td => {
+            const prop = td.getAttribute('data-column-name');
+            const elInput = td.querySelector('input');
+    
+            if (elInput) {
+                data.push({ property: prop, value: elInput.value });
+            }
         });
-
-        const elTdActions = this.makeGridviewContentRowActions(entry);
-        elRow.append(elTdActions);
-
-        return elRow;
+  
+        return data;
     },
 
-    makeGridviewContentRowActions(entry) {
-        const elTdActions = el('td', ['text-nowrap', 'datagrid-actions-cell']);
-        const elActionGroup = el('div', ['d-flex', 'gap-1']);
-        
-        const btnView = el('button', ['btn-action', 'btn-action-view'], { title: 'Visualizar' });
-        btnView.innerHTML = '🔍';
-        
-        const btnEdit = el('button', ['btn-action', 'btn-action-edit'], { title: 'Salvar' });
-        btnEdit.innerHTML = '✏️';
-        btnEdit.addEventListener('click', () => this.commit(entry.id));
-        
-        const btnDelete = el('button', ['btn-action', 'btn-action-delete'], { title: 'Excluir' });
-        btnDelete.innerHTML = '🗑️';
-        btnDelete.addEventListener('click', () => this.delete(entry.id));
-
-        elActionGroup.append(btnView, btnEdit, btnDelete);
-        elTdActions.append(elActionGroup);
-
-        return elTdActions;
-    },
-
-    makeGridviewContentRows() {
-        const rows = [];
-        if(!this.data || this.data === null) return rows;
-
-        this.data.forEach((entry) => {
-            const elRow = this.makeGridviewContentRow(entry);
-            rows.push(elRow);
-        });
-
-        return rows;
-    },
-
-    makeGridviewFooter() {
-        const elFooter = el('div', ['datagrid-footer', 'd-flex', 'justify-content-between', 'align-items-center', 'flex-wrap', 'gap-3']);
-        
-        const elPagination = el('div', ['pagination-container', 'd-flex', 'gap-1']);
-        const btnPrev = el('button', ['btn-page'], { disabled: true }); btnPrev.innerText = 'Previous';
-        const btnP1 = el('button', ['btn-page']); btnP1.innerText = '1';
-        const btnP2 = el('button', ['btn-page', 'active']); btnP2.innerText = '2';
-        const btnP3 = el('button', ['btn-page']); btnP3.innerText = '3';
-        const btnNext = el('button', ['btn-page']); btnNext.innerText = 'Next';
-        elPagination.append(btnPrev, btnP1, btnP2, btnP3, btnNext);
-
-        const elInfo = el('div', ['d-flex', 'align-items-center', 'gap-3', 'text-muted', 'font-size-sm']);
-        elInfo.innerHTML = `<div>Showing 1 to 2 of 2 entries</div>`;
-
-        elFooter.append(elPagination, elInfo);
-        return elFooter;
+    makeDynamic(params) {
+        const result = {};
+        params.forEach(item => { result[item.property] = item.value; });
+        return result;
     },
 
     reloadGridviewContent() {
@@ -404,13 +413,17 @@ export const RegistryComponent = {
         this.appendGridviewContentRows(elTbody);
     },
 
-    // Método auxiliar interno para evitar repetição de código
-    appendGridviewContentRows(container) {
-        container.append(this.makeGridviewContentNew());
-        const rows = this.makeGridviewContentRows();
-        if (rows && rows.length > 0) {
-            rows.forEach(elRow => container.append(elRow));
-        }
+    updateData(entry) {
+        const elTr = document.querySelector(`tr[data-id='${entry.id}']`);
+        if(!elTr || elTr === null) return;
+
+        Object.entries(entry).forEach(([p, val]) => {
+            const elTd = elTr.querySelector(`td[data-column-name=${p}]`);
+            if(elTd) {
+                const elInput = elTd.querySelector('input');
+                if(elInput) elInput.value = val;
+            }
+        });
     },
 
     toLowerCamelCase(str) {
@@ -421,5 +434,8 @@ export const RegistryComponent = {
             .replace(/^([A-Z])/, (match) => match.toLowerCase())
             // Encontra o '_' seguido de uma letra e a transforma em maiúscula
             .replace(/_([a-z0-9])/g, (match, letter) => letter.toUpperCase());
-    }
+    },
+
+    //#endregion
+
 };
