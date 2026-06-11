fa399a1f08cda0aedd1f748eab54cb6154d7c4b6
cfsdev027 <cfsdev.github@gmail.com>
2026-06-11 09:24:19 -0300

    Optimize Supabase client instantiation
    Refactor ServiceSupabase to cache the client instance.

--- src/js/services/service-supabase.js
+++ src/js/services/service-supabase.js
@@ -1,7 +1,10 @@
 import {SUPABASE_URL,SUPABASE_ANON_KEY} from '../config/configurations.js';
 
 export const ServiceSupabase = {
+    c: null,
     client: function(){
-      return window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+        if(!c || c === null) c = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+        return c;
     }
 }
