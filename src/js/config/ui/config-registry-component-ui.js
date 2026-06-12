5bf4e04adf9db8f5c8b2d8741db668d0d40e4824
cfsdev027 <cfsdev.github@gmail.com>
2026-06-12 10:30:31 -0300

    Add constants for gridview pagination IDs

---
+++ src/js/config/ui/config-registry-component-ui.js
@@ -0,0 +1,3 @@
+export const GRIDVIEW_PAGINATION_CURREND_PAGE_ID = 'gridview-pagination-current-page';
+export const GRIDVIEW_PAGINATION_TOTAL_PAGES_ID = 'gridview-pagination-total-pages';
+export const GRIDVIEW_PAGINATION_COUNT_ID = 'gridview-pagination-count';
