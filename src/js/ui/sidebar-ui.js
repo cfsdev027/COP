1836cb7b6df2b197eff8b7546ac6c505e1c1885c
cfsdev027 <cfsdev.github@gmail.com>
2026-06-15 09:32:41 -0300

    Refactor SidebarUI with improved error handling

--- src/js/ui/sidebar-ui.js
+++ src/js/ui/sidebar-ui.js
@@ -74,7 +74,7 @@ export const SidebarUI = {
 
         elNavbarGroup.append(elUserInfoToggler, elLogoutToggler, elNavbarToggler);
 
-        const elNavbarCollapseMobile = el('div', ['collapse', 'navbar-collapse', 'd-block', 'd-md-none', 'w-100'], { id: 'navbar-collapse-mobile' });
+        const elNavbarCollapseMobile = el('div', ['collapse', 'navbar-collapse', 'd-none', 'w-100'], { id: 'navbar-collapse-mobile' });
         const elNavbarCollapseDesktop = el('div', ['collapse', 'navbar-collapse', 'd-none', 'd-md-block', 'w-100'], { id: 'navbar-collapse-desktop' });
         this.makeOptionsMenu(elNavbarCollapseDesktop);
         
